2a643082e0497950ada2339f348d4b220273d823
chris <christopersab@gmail.com>
2024-09-04 08:41:50 +0800

    main

---
+++ src/constants/common.ts
@@ -0,0 +1,15 @@
+export const NAME = 'Christoper Sab';
+
+// ABOUT PAGE
+export const ABOUT_TEXT_1 =
+  'An aspiring Information Techonology Developer that is willing to undergo harsh environment to develop skills and build experience.';
+export const ABOUT_TEXT_2 =
+  'As a Junior Developer, my role involves developing and maintaining web applications from both the frontend and backend. I can collaborate with cross-functional teams to design, implement, and deploy scalable solutions that align with business goals. My focus is on writing clean, efficient code and continuously learning to improve my skills in various technologies and frameworks.';
+export const ABOUT_TEXT_3 =
+  'Junior Developer that is hoping to gain more experiences to solidify my skills in terms of Web Developing and maintaining web-based application.';
+
+// RESUME PAGE
+export const RESUME_TEXT_1 =
+  'Aspiring Software Developer with a passion for building websites and web applications using modern JavaScript tools and frameworks. Eager to grow and gain experience in the field while contributing to impactful projects.';
+export const RESUME_TEXT_2 =
+  'Aspiring Software Developer with a passion for building websites and web applications using modern JavaScript tools and frameworks.';
